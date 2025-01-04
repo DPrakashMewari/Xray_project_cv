2266a3d11ed0bf24a9f121feeafe16ed1a729060
DPrakashMewari <Prakash.mewari@yahoo.com>
2025-01-04 19:09:22 +0530

    Data Ingestion

---
+++ Xray/cloud_storage/s3_operation.py
@@ -0,0 +1,18 @@
+import os 
+import sys 
+from  Xray.exception import XrayException
+
+
+class S3Operation:
+    def sync_folder_s3():     
+        try:
+            pass
+        except Exception as e:
+            raise XrayException(e,sys)
+    def sync_folder_s3():     
+        try:
+            pass
+        except Exception as e:
+            raise XrayException(e,sys)
+
+
---
+++ Xray/components/data_ingestion.py
@@ -0,0 +1,19 @@
+from Xray.cloud_storage.s3_operation import S3Operation
+# from Xray.constant.training_pipeline import *
+from Xray.entity.artifact_entity import DataIngestionArtifact
+from Xray.entity.config_entity import DataIngestionConfig
+from Xray.exception import XRayException
+from Xray.logger import logging
+
+
+class DataIngestion:
+    def __init__(self):
+        pass
+
+    def get_data_from_s3():
+        pass
+
+    def intiate_data_ingestion():
+        pass
+
+    
